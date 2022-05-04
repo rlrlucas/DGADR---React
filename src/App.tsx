6afd515a3908da4a9b37861e4b1bd742b3d7f1f0
Rute Lucas <rute.lucas@gmail.com>
2022-05-04 11:37:26 +0100

    dgadr_iniciail

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React, { useEffect, useState } from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+import Routes from "./routes";
+
+
+
+function App() {
+  return (
+    <Router>
+  
+      <Routes />
+    </Router>
+
+  );
+}
+
+export default App;
